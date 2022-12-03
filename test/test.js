7c2ffa8a3d46b341e831611ff539543600d6b020
Chelsea Pae <chelseapae@gmail.com>
2022-12-02 20:51:30 -0800

    Add more tests

--- test/test.js
+++ test/test.js
@@ -20,6 +20,54 @@ describe('All answers', (done) => {
   })
 })
 
+describe('Post Question', (done) => {
+  test('It should return 200 when posting a question', async () => {
+    moxios.stubRequest(`http://localhost:${process.env.PORT}/qa/questions`, {
+      status: 200
+    })
+  })
+})
+
+describe('Post Answer', (done) => {
+  test('It should return 200 when posting an answer', async () => {
+    moxios.stubRequest(`http://localhost:${process.env.PORT}/qa/questions/71717/answers`, {
+      status: 200
+    })
+  })
+})
+
+describe('Put Question as helpful', (done) => {
+  test('It should return 200 when marking a question as helpful', async () => {
+    moxios.stubRequest(`http://localhost:${process.env.PORT}/qa/questions/600/helpful`, {
+      status: 200
+    })
+  })
+})
+
+describe('Put Answer as helpful', (done) => {
+  test('It should return 200 when marking annswer as helpful', async () => {
+    moxios.stubRequest(`http://localhost:${process.env.PORT}/qa/answers/600/helpful`, {
+      status: 200
+    })
+  })
+})
+
+describe('Put Question to report', (done) => {
+  test('It should return 200 when reporting a question', async () => {
+    moxios.stubRequest(`http://localhost:${process.env.PORT}/qa/questions/600/report`, {
+      status: 200
+    })
+  })
+})
+
+describe('Put Answer to report', (done) => {
+  test('It should return 200 when reporting an answer', async () => {
+    moxios.stubRequest(`http://localhost:${process.env.PORT}/qa/answers/600/report`, {
+      status: 200
+    })
+  })
+})
+
 // function sum(a, b) {
 //   return a + b;
 // }
